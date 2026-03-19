a2cff16935f2a1ddad77d61663191c5c1e63c4e3
Sam <doansamquoc@gmail.com>
2026-03-19 14:51:18 +0700

    feat: update landing page

--- src/routes/_landing.tsx
+++ src/routes/_landing.tsx
@@ -1,13 +1,20 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute, Link, useLocation } from "@tanstack/react-router";
 import { ArrowRight, Icon } from "lucide-react";
 import { waveCircle } from "@lucide/lab";
 import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
 
 export const Route = createFileRoute("/_landing")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { pathname } = useLocation();
+  const navLinks = [
+    { to: "/", label: "Tổng quan" },
+    { to: "/about", label: "Về chúng tôi" },
+  ];
+
   return (
     <div>
       <nav className='p-2 flex gap-3 items-center'>
@@ -15,12 +22,22 @@ function RouteComponent() {
           Flow <Icon iconNode={waveCircle} />
         </Link>
 
-        <Link to='/' className='[&.active]:underline'>
-          Overview
-        </Link>
-        <Link to='/about' className='[&.active]:underline'>
-          About
-        </Link>
+        {navLinks.map((link, index) => (
+          <Link
+            key={index}
+            to={link.to}
+            className='relative py-1 text-sm font-medium transition-colors hover:text-primary'
+          >
+            {link.label}
+            {pathname === link.to && (
+              <motion.div
+                layoutId='underline'
+                className='absolute bottom-0 left-0 right-0 h-1 bg-blue-600'
+                transition={{ type: "spring", stiffness: 380, damping: 10 }}
+              />
+            )}
+          </Link>
+        ))}
 
         <Button className='ml-auto' asChild>
           <Link to='/login'>
