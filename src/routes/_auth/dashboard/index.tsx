c85d7d8ca1598c9c4d22e066240e959c1cf2facd
Sam <doansamquoc@gmail.com>
2026-03-20 21:17:32 +0700

    feat: add flatten user from privyUser

--- src/routes/_auth/dashboard/index.tsx
+++ src/routes/_auth/dashboard/index.tsx
@@ -1,20 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { flattenUser } from "@/utils/flatten-user";
 import { usePrivy } from "@privy-io/react-auth";
 import { createFileRoute } from "@tanstack/react-router";
+import { useMemo } from "react";
 
 export const Route = createFileRoute("/_auth/dashboard/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { logout, user } = usePrivy();
+  const { logout, user: privyUser } = usePrivy();
+  const user = useMemo(() => flattenUser(privyUser), [privyUser]);
   console.log("User: ", user);
   return (
     <div>
-      <Button onClick={logout}>
-        Logout,{" "}
-        {user?.linkedAccounts.find((account) => "email" in account)?.email}
-      </Button>
+      <Button onClick={logout}>Logout, {user?.email}</Button>
     </div>
   );
 }
---
+++ src/types/user.type.ts
@@ -0,0 +1,8 @@
+export type User = {
+  id: string;
+  email?: string;
+  name?: string;
+  eoaAddress?: string;
+  smartAddress?: string;
+  loginType: string;
+};
